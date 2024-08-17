8029574e1e008333a69148a3ef25311e56826e35
kko <kkl24@mail.ru>
2024-08-17 14:59:45 +0300

    4

--- src/test/java/MathOpsTest.java
+++ src/test/java/MathOpsTest.java
@@ -1,8 +1,29 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class MathOpsTest{
+    private final dz13_08.MathOps o=new dz13_08.MathOps();
+
     @Test
     public void sum(){
-        System.out.println("msg from test");
+        assertEquals(o.sum(2,4),6); 
+        System.out.println("msg from sum()");
+    }
+    @Test
+    public void sub(){
+        assertEquals(o.sub(2,4),-2); 
+        System.out.println("msg from sub()");
+    }
+    @Test
+    public void mul(){
+        assertEquals(o.mul(2,4),8); 
+        System.out.println("msg from mul()");
+    }
+    @Test
+    public void div(){
+        assertEquals(o.div(4,2),2); 
+        System.out.println("msg from div()");
     }
 }
